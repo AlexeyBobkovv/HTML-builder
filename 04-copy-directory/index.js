5f97a4fbba026f6b103e48faa703d7354710a689
Just_Alexey <60109266+AlexeyBobkovv@users.noreply.github.com>
2022-05-26 07:54:55 +0300

    fix 4-th exercise

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,46 +1,19 @@
-const fs = require('fs');
-const path = require('path');
+let path = require("path");
+let fs = require("fs/promises");
+let mainDir = path.join(__dirname, "files");
+let copyDir = path.join(__dirname, "files-copy");
 
-const mainFolderPath = path.join(__dirname, 'files');
-const copiedFolderPath = path.join(__dirname, 'files-copy');
-
-function copyDir(mainFolderPath, copiedFolderPath) {
-  fs.mkdir(copiedFolderPath, { recursive: true }, (err) => {
-    errorHandle(err);
+fs.rm(copyDir, { recursive: true, force: true }).finally(function () {
+  fs.mkdir(copyDir, {
+    recursive: true,
   });
-
-  fs.readdir(mainFolderPath, { withFileTypes: true }, (err, files) => {
-    errorHandle(err);
-
-    files.forEach(file => {
-      const mainFilePath = path.join(mainFolderPath, file.name);
-      const copiedMainFilePath = path.join(copiedFolderPath, file.name);
-
-      fileHander(file, mainFilePath, copiedMainFilePath);
-      dirHander(file, mainFilePath, copiedMainFilePath);
+  fs.readdir(mainDir, { withFileTypes: true }).then(function (files) {
+    files.forEach(function (file) {
+      if (file.isFile()) {
+        let mainFileDir = path.join(mainDir, file.name);
+        let copyFileDir = path.join(copyDir, file.name);
+        fs.copyFile(mainFileDir, copyFileDir);
+      }
     });
   });
-}
-
-function errorHandle(err) {
-  if (err) {
-    return console.error(err);
-  }
-}
-
-function fileHander(file, mainFilePath, copiedmainFilePath) {
-  if (file.isFile()) {
-    fs.copyFile(mainFilePath, copiedmainFilePath, (err) => {
-      errorHandle(err);
-    });
-  }
-}
-
-function dirHander(file, mainFilePath, copiedmainFilePath) {
-  if (file.isDirectory()) {
-    copyDir(mainFilePath, copiedmainFilePath);
-  }
-}
-
-
-copyDir(mainFolderPath, copiedFolderPath);
+});
